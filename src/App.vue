6efbd5d45ec4b4dbffbc4ad2dc94a80a9f279292
fuhang <1017818004@qq.com>
2020-12-03 15:09:23 +0800

    interceptor

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,17 @@
 </template>
 
 <script>
-
+// import storage from './storage/index';
 export default {
   name: 'app',
   components: {
-
+  },
+  mounted() {
+    // storage.setItem('a',1);
+    // storage.setItem('b',2);
+    // storage.setItem('usr',{'c':1});
+    // storage.setItem('abc',{'d':1}, 'usr');
+    // storage.clear('c', 'usr');
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
+import axios from 'axios'
+import vueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+//错误拦截
+axios.interceptors.response.use(function (response){
+  let res = response.data;
+  if(res.status == 0){
+    return res.data;
+  }else if(res.status == 10){
+    window.location.href('/#/login');
+  }else{
+    alert(res.msg);
+  }
+})
+Vue.use(vueAxios, axios);
 
 Vue.config.productionTip = false
 
